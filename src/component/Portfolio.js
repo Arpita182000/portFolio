a972fda0eff87f63523ab10c09615c0b7fe3cc15
Arpita Sarkar <sarkararpita182000@gmail.com>
2024-05-31 20:40:50 +0530

    c

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import img from "../Assets/img/img.jpeg";
 import p from "../Assets/img/p.png";
 import y from '../Assets/img/Screenshot (38).png'
+import m from '../Assets/img/m.png'
+import f from '../Assets/img/f.png'
 function Portfolio() {
   return (
     <>
@@ -23,15 +25,26 @@ function Portfolio() {
             </Link>
           </div>
         </div>
-        <div class="section">
+        
           <div class="box">
             <h1>YouTube</h1>
             <Link to="https://you-tube-eta.vercel.app/" target="_blank">
               <img src={y} width={350} />
             </Link>
           </div>
-          <div class="box">Small fronten Projects</div>
-        </div>
+          <div class="box">
+          <h1>Small Projects</h1>
+            <Link to="https://monsta-oix1c6ae4-arpitas-projects-9e2857b5.vercel.app/" target="_blank">
+              <img src={m} width={350} />
+            </Link>
+          </div>
+        
+          <div class="box">
+          <h1>Small Projects</h1>
+            <Link to="https://face-book-mjubgoyus-arpitas-projects-9e2857b5.vercel.app/" target="_blank">
+              <img src={f} width={350} />
+            </Link>
+          </div>
       </div>
     </>
   );
